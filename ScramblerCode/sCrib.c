4d688efa36ff01d49591c273e20f6dece80b8747
Dan Cvrcek <dan@s-crib.com>
2014-03-13 10:10:17 +0000

    Update sCrib.c
    adding license

--- ScramblerCode/sCrib.c
+++ ScramblerCode/sCrib.c
@@ -1,3 +1,12 @@
+/*****************************************************************************
+Copyright 2013-14, Smart Crib Ltd
+@license: This code is provided for review and analysis only. It must not be 
+          used for any other purpose without permission of Smart Crib Ltd
+@version: 1.0
+@email: info@s-crib.com
+@status: Test
+******************************************************************************/
+
 ///////////////////////////////////////////////////////////////////////////////
 /////// THE MAIN PROCESSING LOOP
 ///////////////////////////////////////////////////////////////////////////////	
