994d29aee0912cd3e15e3cf299552ac03dbce2c9
Dan Cvrcek <dan@s-crib.com>
2014-06-20 15:01:18 +0100

    logging added to Dongle.py

--- sCribManager/Dongle.py
+++ sCribManager/Dongle.py
@@ -17,6 +17,7 @@ import time
 from pylibftdi import Driver,Device
 import sys
 import traceback
+from shmLogging import log_trace, log_use
 
 class Dongle(object):
     '''
@@ -147,8 +148,7 @@ class Dongle(object):
         self._errorMSg = ""
         self._stick = None
         if deviceId == "":
-            print("Dongle constructor - no deviceID provided.")
-            traceback.print_stack()
+            log_trace('C', '0067', "No deviceID provided", traceback=traceback.print_stack())
             pass #self.stick = Device(mode = "t")
         else:
             try:
@@ -156,10 +156,8 @@ class Dongle(object):
                 self._stick.open()
                 #one call of the baudrate setter does not always work 
                 self._stick.baudrate = 3000000
-                self._stick.baudrate = 3000000
-                self._stick.baudrate = 3000000
             except:
-                print("Dongle constructor - error when opening %s - %s"%(deviceId, sys.exc_info()[0]))
+                log_trace('E', '0068', "Error when opening new device", ID=deviceId, exception=', '.join(sys.exc_info()[0]))
                 if self._stick:
                     self._stick.close()
                 self._stick = None
@@ -170,6 +168,7 @@ class Dongle(object):
             if self._ID:
                 self._ID = self._ID.split()[0]
             else:
+                log_trace('E', '0069', "GETID() returned no data", HWID=deviceId)
                 self._stick.close()
                 self._stick = None
                 # ERR405
@@ -179,16 +178,19 @@ class Dongle(object):
             if self._Cluster:
                 self._Cluster = self._Cluster.split()[0]
             else:
+                log_trace('E', '0070', "GETCLUSTER() returned no data", HWID=deviceId)
                 self._stick.close()
                 self._stick = None
                 #ERR406
                 return
             
             if (self._ID[:3]=="ERR") or (self._Cluster[:3]=="ERR"):
+                log_trace('E', '0071', "Device initialisation returned an error (GETID or GETCLUSTER)", HWID=deviceId, ID=self._ID, cluster=self._Cluster)
                 self._stick.close()
                 self._stick = None
 
     def exists(self):
+        log_trace('D', '0072', "exists() called", ID=self._ID)
         if self._stick:
             return True
         else:
@@ -201,7 +203,7 @@ class Dongle(object):
     '''
     @staticmethod  
     def listDevices():
-
+        log_trace('D', '0073', "listDevices() called", detail="n/a")
         dev_list = []
         dev_dict = {}
         devices = None
@@ -210,28 +212,31 @@ class Dongle(object):
             devices =  Driver().list_devices()
         except AttributeError, e:
             # ERR_ENUMERATE ERR403
-            print("Error when enumerating devices - Attribute Error (%s)"%e)
+            log_trace('E', '0074', "Error when enumerating devices - Attribute Error", exception=e)
             if  not devices:
                 return None
         except :
             # ERR_DRIVER ERR404
-            print("It is not possible to list devices. Make sure the correct driver is assigned to S-Crib Scrambler (%s)"%sys.exc_info()[0])
+            log_trace('E', '0075', "Error when enumerating devices", exception=', '.join(sys.exc_info()[0]))
             if not devices:
                 return None
 
         # expected response is an empty list - None may mean an error
         if not devices:
+            log_trace('E', '0076', "list_devices() returned 'None'", detail="n/a")
             return None
 
         for device in devices:
             device = map(lambda x: x.decode('latin1'), device)
             vendor, product, serial = device
             dev_list.append(("%s:%s:%s" % (vendor, product, serial),serial))
+            log_trace('D', '0077', "Device found", vendor=vendor, product=product, serial=serial)
             dev_dict[serial] = 1
         return (dev_list, dev_dict)
 
 
     def _encrypt(self, message, passphrase):
+        log_trace('D', '0078', "_encrypt() called", detail="n/a")
         # passphrase MUST be 16, 24 or 32 bytes long, how can I do that ?
         IV =  "\x00" * self._BLOCK_SIZE
         while (len(passphrase)<32):
@@ -241,6 +246,7 @@ class Dongle(object):
         return aes.encrypt(message)
 
     def _decrypt(self, encrypted, passphrase):
+        log_trace('D', '0079', "_decrypt() called", detail="n/a")
         IV = "\x00" * self._BLOCK_SIZE
         while (len(passphrase)<32):
             passphrase = passphrase + "\x00"
@@ -258,6 +264,7 @@ class Dongle(object):
         
 
     def _writeCmd(self, cmd):
+        log_trace('D', '0080', "_writeCmd() called", detail="n/a") 
         try:
             self._stick.flush()
             #time.sleep(0.1)
@@ -265,12 +272,13 @@ class Dongle(object):
             #time.sleep(0.1)
             return length
         except:
-            print("Device unplugged %s - write"% self.ID())
+            log_trace('E', '0081', "Device unplugged when writing", ID=self._ID())
             #ERR410
             return None
 
 
     def _readData(self,replyLength):
+        log_trace('D', '0082', "_readData() called", detail="n/a")
         try:
             data = ""
             started = time.time()
@@ -287,17 +295,17 @@ class Dongle(object):
                     break
             if delta > self._READ_TIMEOUT:
                 #ERR411
-                print("Timeout on reading - dongle %s (%f seconds)"%(self._HWID, delta))
+                log_trace('E','0083', "Timeout on reading", HWID=self._HWID, timeout=delta)
                 return None
             #if data[0:3] == "ERR":
             #    self.errorFlag = True
             #    self.errorMSg = data
             #    #self.stick.flush()
             #    #time.sleep(0.25)
-            print("Data %s read in %f seconds"%(data.strip(), delta))
+            log_trace('D', '0084', "Data read from device", data=data.strip(), latency=delta)
             return data
         except:
-            print("No data to read from device %s"%self.ID())
+            log_trace('E', '0085', "Error when reading from device", ID=self._ID,exception=', '.join(sys.exc_info()[0]))
             #ERR409
             return None
         
@@ -325,43 +333,56 @@ class Dongle(object):
         self._stick.close()
     
     def reopen(self):
+        log_trace('D', '0086', "reopen() called", HWID=self._HWID)
         self._stick.close()
         self._stick.open()
 #    Management Commands 
 
     def GETINITKEY(self):
+        log_trace('D', '0087', "GETINITKEY() called", ID=self._ID)
+        start=time.time()
         self.firstCmd = False
         (_,result) = (self._writeCmd((self.getInitKeyText)),self._readData(self.getInitKeyTextLen))
+        latency=time.time()-start
+        log_use('0009', "GETINITKEY", self._ID, "n/a", latency=latency)
         if result:
             return result
         else:
             return None
         
     def GETPASSWD(self, index):
-        
+        log_trace('D', '0088', "GETPASSWD() called", ID=self._ID, index=index)
+        start = time.time()
         try:
             passwordIndex = int(index,10)
         except:
-            print("Password index must be an Integer")
+            log_trace('D', '0089', "GETPASSWD - wrong parameter", ID=self._ID, index=index)
             return "ERR104"
 
         if passwordIndex >= 2 and passwordIndex <= 4:
             self._stick.flush()
             (_,result) = (self._writeCmd((self.getPasswordText %passwordIndex)),self._readData(self.getPasswordTextLen))
+            latency = time.time() - start
+            log_use('0010', "GETPASSWD", self._ID, "n/a", latency=latency)
             if result:
                 return result
             else:
                 return None
         else:
-            print("Password index must be 2/3/4 not: %d" %passwordIndex)
+            log_trace('D', '0090', "GETPASSWD - wrong parameter", ID=self._ID, index=index)
             return "ERR103"
 
 #    Status Commands
         
 
     def GETID(self):
+        log_trace('D', '0091', "GETID() called", ID=self._ID)
+        start = time.time()
+
         self.firstCmd = False
         (_, result) = (self._writeCmd(self.getIdText),self._readData(self.getIdTextLen))
+        latency = time.time() - start
+        log_use('0011', "GETID", self._ID, "n/a", latency=latency)
         # add logging here
         if result:
             dongle = result
@@ -372,8 +393,12 @@ class Dongle(object):
         
 
     def GETCLUSTER(self):
+        log_trace('D', '0092', "GETCLUSTER() called", ID=self._ID)
+        start = time.time()
         self.firstCmd = False
         (_, result) = (self._writeCmd(self.getClusterText),self._readData(self.getClusterTextLen))
+        latency = time.time() - start
+        log_use('0012', "GETCLUSTER",  self._ID, "n/a",  latency=latency)
         # add logging here
         if result:
             return result
@@ -383,18 +408,25 @@ class Dongle(object):
 
 
     def GETLOCKED(self):
+        log_trace('D', '0093', "GETLOCKED() called", ID=self._ID)
+        start = time.time()
         self.firstCmd = False
         (_, result) = (self._writeCmd(self.getLockedText),self._readData(self.getLockedTextLen))
         # add logging here
+        latency = time.time() - start
+        log_use('0013', "GETLOCKED",  self._ID, "n/a", latency=latency)
         if result:
             return result
         else:
             return None
 
     def ENGETID(self, dongleID, data):
-        self.firstCmd = False
+        log_trace('D', '0094', "ENGETID() called", ID=self._ID, dongle = dongleID, data=data)
+        start = time.time()
         (_, result) = (self._writeCmd(self.enGetIdText%data),self._readData(self.enGetIdTextLen))
         # add logging here
+        latency = time.time() - start
+        log_use('0014', "ENGETID",  self._ID, "n/a",  dongle=dongleID, data=data, latency=latency)
         if result:
             enID = result
         else:
@@ -402,8 +434,11 @@ class Dongle(object):
         return enID
         
     def GETCOUNTER(self):
-        self.firstCmd = False
+        log_trace('D', '0095', "GETCOUNTER() called", ID=self._ID)
+        start = time.time()
         (_, result) = (self._writeCmd(self.getCounterText),self._readData(self.getCounterTextLen))
+        latency = time.time() - start
+        log_use('0015', "GETCOUNTER",  self._ID, "n/a",  latency=latency)
         if result:
             result  = result
         else:
@@ -411,8 +446,11 @@ class Dongle(object):
         return result
         
     def GETDELAY(self):
-        self.firstCmd = False
+        log_trace('D', '0096', "GETDELAY() called", ID=self._ID)
+        start = time.time()
         (_, result) =  (self._writeCmd(self.getDelayText),self._readData(self.getDelayTextLen))
+        latency = time.time() - start
+        log_use('0016', "GETDELAY",  self._ID, "n/a", latency=latency)
         if result:
             result  = result
         else: 
@@ -421,47 +459,51 @@ class Dongle(object):
 #    Execution Commands
     
     def SCRAMBLE(self, password, saltLength=0, salt=""):
-        self.firstCmd = False
+        log_trace('D', '0097', "SCRAMBLE() called", ID=self._ID)
+        start = time.time()
         if self._isASCII(password):
             if isinstance(saltLength,(int)):
                 if saltLength >=0 and saltLength <=32:
                     if salt == "" or len(salt) == saltLength:
                         (_, result) =  (self._writeCmd(self.scrambleText %(password,saltLength,salt)),
                                 self._readData(self.getDelayTextLen + saltLength + self.scrambleLength ))
+                        latency = time.time() - start
+                        log_use('0017', "SCRAMBLE",  self._ID, "n/a", latency=latency)
                         if (result):
                             result  = result
                         else:
                             result = None
                         return result
-                    
-                    print(ValueError("Salt length : %d does not match the length of the salt supplied: %s" %(saltLength,len(salt))))
+                    log_trace('D', '0098', "Salt length does not match the length of the salt supplied.", saltLength=saltLength, salt=salt) 
                     return "ERR105"
                 else:
-                    print("Salt length must be between 0 and 32 not: %d" %saltLength)
+                    log_trace('D', '0099', "Salt length must be between 0 and 32", ID=self._ID, saltLength=saltLength)
                     return "ERR106"
             else:
-                print("Salt Length must be an Integer")
+                log_trace('D', '0100', "Salt length must be integer", ID=self._ID, saltLength=saltLength)
                 return "ERR107"
         else:
-            print("Password must be in ASCII")
+            log_trace('D', '0101', "Password must be in ASCII encoding", ID=self._ID)
             return "ERR108"
         
         
     def _ENSCRAMBLE(self, passphrase, serverCounter, password, saltLength=0, salt=""):
         # create the encrypted packet
+        log_trace('D', '0102', "Test _ENSCRAMBLE() called", ID=self._ID)
+        start = time.time()
         if len(serverCounter)>10:
-            print("Server counter can be only 10 characters long - ENSCRAMBLE")
+            log_trace('D', '0103', "Server counter can be only 10 characters long - ENSCRAMBLE", ID=self._ID, counter=serverCounter)
             return "ERR109"
             
         message = serverCounter.ljust(10) + " " + password.strip() + " "
         if not isinstance(saltLength, (int)):
-            print("salt length must be an INT")
+            log_trace('D', '0104', "Salt length must be integer", ID=self._ID, saltLength=saltLength)
             return "ERR107"
         if saltLength<0 or saltLength > 16: 
-            print("salt length must be between 0 and 16 not: %s" % saltLength)
+            log_trace('D', '0105', "Salt length must be between 0 and 16", ID=self._ID, saltLength=saltLength)
             return "ERR106"
         if (saltLength != len(salt)) and (len(salt)>0):
-            print("salt is not of expected length" % saltLength)
+            log_trace('D', '0106', "Salt length does not match the length of the salt supplied", ID=self._ID, saltLength=saltLength, salt=salt)
             return "ERR105"
 
         message = message + "%02d"%saltLength
@@ -479,13 +521,15 @@ class Dongle(object):
 
         # call the dongle
         (_, result) = (self._writeCmd(self.enScrambleText % dataToSend), self._readData(self.enScrambleTextLen ))
+        latency = time.time() - start
+        log_use('0018', "Test _ENSCRAMBLE",  self._ID, "n/a", latency=latency)
         result = result.encode("ascii","ignore")
         # the result must be terminated with '\n'
         data = binascii.unhexlify(result.strip())
         result = self._decrypt(data, passphrase)
         if (len(result)>0):
             if (serverCounter.ljust(11)!=result[:11]): #check the server challenge
-                print("Server counter is incorrect IN: %s, OUT %s " % (serverCounter.ljust(10),result[:10]))
+                log_trace('D', '0107', "Server counter is incorrect", ID=self._ID, counterIn=serverCounter.ljust(10), counterOut=result[:10])
                 return "ERR301"
 
             index = 11
@@ -496,22 +540,25 @@ class Dongle(object):
             counter = (((ord(result[index])*256+ord(result[index+1]))*256+ord(result[index+2]))*256+ord(result[index+3]))
             return (password, salt, counter)
         else:
-            print("No data returned")
+            log_trace('D', '0108', "No data returned", ID=self._ID, counter=serverCounter, saltLength=saltLength, salt=salt, password=password, pwd4=passphrase)
             return "ERR302"
         
                 
     def ENSCRAMBLE(self,request):
+        log_trace('D', '0109', "ENSCRAMBLE() called", ID=self._ID)
+        start = time.time()
         self.firstCmd = False
         if len(request)==128:
             (_, result) = (self._writeCmd((self.enScrambleText %request)),self._readData(self.enScrambleTextLen))
-            result  = result
+            latency = time.time() - start
+            log_use('0019', "ENSCRAMBLE",  self._ID, "n/a", latency=latency)
             return result
         else:
-            print("Encrypted block must be 128 characters long")
+            log_trace('D', '0110', "Request data must be 128 bytes long", ID=self._ID, request=request)
             return "ERR130"
                 
     def process(self, request):
-        
+        log_trace('D', '0111', "process() called", ID=self._ID, request=request) 
         request_bits = request.split()
         command = int(request_bits[0])
         try:
@@ -536,6 +583,7 @@ class Dongle(object):
                 data = self.GETLOCKED()
             elif command == 10: #'SCRAMBLE':10,
                 if len(request)<3:
+                    log_trace('D', '0112', "SCRAMBLE - incorrect number of params", params=len(request))
                     data = "ERR111"
                 else:
                     saltLength = int (request_bits[2])
@@ -544,15 +592,16 @@ class Dongle(object):
                     elif len(request_bits)==4:
                         data = self.SCRAMBLE(request_bits[1], saltLength, request_bits[3])
                     else:
+                        log_trace('D', '0113', "SCRAMBLE - incorrect number of params", params=len(request))
                         data = "ERR111"
             elif command == 11: #'ENSCRAMBLE':11,
                 data = self.ENSCRAMBLE(request_bits[1])
             else:
+                log_trace('D', '0114', "Unknown command", command=command)
                 data = "ERR112"
         except:
+            log_trace('E', '0115', "Exception in process()", exception=', '.join(sys.exc_info()[0]), request=request)
             data = "ERR113"
             
         return data
-
-
         
